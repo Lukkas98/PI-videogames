875c6b264d42ea2fdf97ac2f50d44a69d2cf1528
Lukkas98 <lucaspalma988@gmail.com>
2023-03-02 11:47:08 -0300

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes, useLocation } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage.jsx';
+import Nav from './components/Nav/Nav';
+import Form from './components/Form/Form';
+import Detail from './components/Detail/Detail';
 
 
 function App() {
+
+  const { pathname } = useLocation();
+
   return (
     <>
-      <Link to="/home">AL HOME</Link>
+      <Nav></Nav>
+
       <Routes>
         <Route path="/home" element={<HomePage />}/>
+        <Route path='/create' element={<Form />}/>
+        <Route path='/detail' element={<Detail />}/>
         
       </Routes>
     </>
