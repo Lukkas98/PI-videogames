62bb6c3769b5f0426ff4a730a2f5b5a4c8a800c2
Lukkas98 <lucaspalma988@gmail.com>
2023-03-04 20:19:28 -0300

    bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,45 @@
-import './App.css';
-import { Link, Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage.jsx';
 import Nav from './components/Nav/Nav';
 import Form from './components/Form/Form';
 import Detail from './components/Detail/Detail';
+// import axios from "axios";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllGames } from './redux/actions.js';
+
 
 
 function App() {
 
-  const { pathname } = useLocation();
+  // const { pathname } = useLocation();
+  const dispatch = useDispatch();
+  // const [videogames, setVideogames] = useState([])
+  
+  const videogames = useSelector(state => state.allVideogames)
+  
+  useEffect( ()=>{
+    dispatch(getAllGames());
+  }, [dispatch]);
+  
+  // useEffect(()=>{
+  //   console.log(videogames);
+  // }, [videogames]);
+
+  const searchGame = async (id)=>{
+    // const {data} = await axios(`http://localhost:3001/videogames/${id}`);
+    // setVideogames( oldValues => [...oldValues, data]);
+    // dispatch(addGame(data))
+  }
 
   return (
     <>
-      <Nav></Nav>
+      <Nav searchGame={searchGame}></Nav>
 
       <Routes>
-        <Route path="/home" element={<HomePage />}/>
+        <Route path="/home" element={<HomePage videogames={videogames}/>}/>
         <Route path='/create' element={<Form />}/>
-        <Route path='/detail' element={<Detail />}/>
+        <Route path='/detail/:id' element={<Detail />}/>
         
       </Routes>
     </>
